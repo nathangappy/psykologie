f3650463f8cb379527b0211c0a0a8560faffa87d
nathangappy <nate.gappy@gmail.com>
2020-04-03 10:12:21 -0400

    final changes

--- app.js
+++ app.js
@@ -51,6 +51,6 @@ app.post('/subscribe', async (req, res) => {
 
 const PORT = process.env.PORT || 3000
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('Server is up on port 3000');
 });
